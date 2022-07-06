fa4001de4284bd397d7de190050d32d4f3be2c07
Thomas Robveille <thomasrobveille@gmail.com>
2022-07-06 09:48:01 +0200

    Correction Affichage des datas sur index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,13 +5,27 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
+        link.href = id;
+        article.appendChild(link)
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        link.appendChild(img);
+        link.appendChild(h2);
+        const adresse = document.createElement('div');
+        adresse.innerText = `${city}, ${country}`;
+        const citation = document.createElement('div');
+        citation.innerText = `${tagline}`;
+        const prix = document.createElement('div');
+        prix.innerText = `${price}€`;
+
+        const elements = [link, adresse, citation, prix];
+        elements.forEach(element => article.appendChild(element));
+        
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }

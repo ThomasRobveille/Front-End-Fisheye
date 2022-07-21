17c9f6a941ab80058e9aa9fa3500c438507cf8e6
Thomas Robveille <thomasrobveille@gmail.com>
2022-07-21 10:52:55 +0200

    ajout du portfolio des photographes et correction CSS

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,6 +7,14 @@ function getPhotographers() {
   return photographers;
 }
 
+function getPhotos() {
+  const photos = fetch('./data/photographers.json')
+                          .then(data => data.json())
+                          .then(data => data.media)
+                          .catch(error => console.log("Problème de récupération des photos", error ));
+  return photos;
+}
+
 async function displayData(photographer) {
   const header = document.querySelector('.photograph-header');
   const photographerModel = photographerFactory(photographer);
@@ -14,11 +22,27 @@ async function displayData(photographer) {
   header.appendChild(photographHeader);
 }
 
+function displayPhotos(name, photos) {
+  const photosContainer = document.querySelector('.photos-container');
+
+  photos.forEach(photo => {
+    const photosModel = photosFactory(name, photo);
+    const photosDOM = photosModel.getUserCardDOM();
+    photosContainer.appendChild(photosDOM);
+  });  
+}
+
 async function getPhotographeData() {
   const photographers = await getPhotographers();
   const id = window.location.href.split('?')[1];
   const photographer = photographers.filter(photographer => photographer.id == id)[0];
   displayData(photographer);
+
+  const photos = await getPhotos();
+  const photographePhotos = photos.filter(photo => photo.photographerId == id);
+  console.log(photographePhotos);
+  const name = photographer.name;
+  displayPhotos(name, photographePhotos);
 };
 
 getPhotographeData();
